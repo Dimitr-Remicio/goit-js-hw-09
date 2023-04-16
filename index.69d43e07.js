4c3877680a4484caf1b8c517d0062c70d8b07a6b
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-04-16 01:10:39 +0000

    Deploying to gh-pages from @ Dimitr-Remicio/goit-js-hw-09@4fe4bb62d9a2bfb9f93c6d27c7a226ff1738ed32 🚀

---
+++ index.69d43e07.js
@@ -0,0 +1,2 @@
+var loader=document.querySelector(".loader_page"),timerId=null;function hidenItem(){console.log("hello"),timerId=setInterval((function(){loader.classList.add("loadhide")}),3100)}hidenItem();
+//# sourceMappingURL=index.69d43e07.js.map
